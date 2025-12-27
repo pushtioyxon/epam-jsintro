254306d7710dc3565b02c3d4720f1ebd580a9571
pushtioyxon <pushtioyxon@gmail.com>
2025-12-27 17:42:43 +0500

    Initial commit with authors and invite function

---
+++ script.js
@@ -0,0 +1,11 @@
+const authors = [
+  { name: "Author1", email: "author1@example.com" },
+  { name: "Author2", email: "author2@example.com" },
+  { name: "Author3", email: "author3@example.com" }
+];
+
+// Example function that might be used in tests
+function inviteParticipants() {
+  console.log("Participants invited!");
+}
+
